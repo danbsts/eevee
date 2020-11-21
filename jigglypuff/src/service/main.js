ef7161119813df843c4267aeb7f9b25c60c12d37
Rodrigo Rodrigues <rfrl@cin.ufpe.br>
2020-11-21 18:47:59 -0300

    Add front and service

---
+++ jigglypuff/src/service/main.js
@@ -0,0 +1,50 @@
+const { webSocket } = require('rxjs/webSocket');
+(global).WebSocket = require('ws');
+var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const subject = webSocket("ws://127.0.0.1:10003/traficData");
+const ponytaUrl = "http://127.0.0.1:10077"
+
+var traficData = [];
+var trapData = [];
+var siteData = [];
+
+getSiteList();
+getTrapList(siteData[0].id);
+
+subject.subscribe(
+  batchTraficData => {
+    console.log("PURO " + batchTraficData.toString())
+    console.log('Data received: ' + JSON.stringify(batchTraficData));
+  },
+  err => console.log(err)
+);
+
+function getSiteList(){
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open( "GET", ponytaUrl + `/sites/`, false );
+  xmlHttp.send( null );
+  var jsonResult = JSON.parse(xmlHttp.responseText);
+  console.log("Site list: " + JSON.stringify(jsonResult, null, 2));
+  siteData = jsonResult;
+}
+
+function getTrapList(siteId){
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open( "GET", ponytaUrl + `/sites/${siteId}/`, false );
+  xmlHttp.send( null );
+  var jsonResult = JSON.parse(xmlHttp.responseText);
+  console.log("Trap list: " + JSON.stringify(jsonResult, null, 2));
+  sentCharizardIdList(jsonResult);
+  siteData = jsonResult;
+  return jsonResult;
+}
+
+function sentCharizardIdList(trapData){
+  var idList = [];
+  for (trap in trapData){
+    idList.push(trap.id);
+  }
+  subject.send({
+    ids: idList
+  }); 
+}
